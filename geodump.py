a700c5882c63c94654c9363ca5f300d3a74377a1
Vivek Kumar Mishra <68559766+Vivek-K-M@users.noreply.github.com>
2020-07-25 00:03:59 +0530

    Add files via upload

---
+++ geodump.py
@@ -0,0 +1,45 @@
+# Once you have some data loaded into geodata.sqlite, you can
+# visualize the data using the (geodump.py) program.  This
+# program reads the database and writes tile file (where.js)
+# with the location, latitude, and longitude in the form of
+# executable JavaScript code.
+
+
+import sqlite3
+import json
+import codecs
+
+conn = sqlite3.connect('geodata.sqlite')
+cur = conn.cursor()
+
+cur.execute('SELECT * FROM Locations')
+fhand = codecs.open('where.js', 'w', "utf-8")
+fhand.write("myData = [\n")
+count = 0
+for row in cur :
+    data = str(row[1].decode())
+    try: js = json.loads(str(data))
+    except: continue
+
+    if not('status' in js and js['status'] == 'OK') : continue
+
+    lat = js["results"][0]["geometry"]["location"]["lat"]
+    lng = js["results"][0]["geometry"]["location"]["lng"]
+    if lat == 0 or lng == 0 : continue
+    where = js['results'][0]['formatted_address']
+    where = where.replace("'", "")
+    try :
+        print(where, lat, lng)
+
+        count = count + 1
+        if count > 1 : fhand.write(",\n")
+        output = "["+str(lat)+","+str(lng)+", '"+where+"']"
+        fhand.write(output)
+    except:
+        continue
+
+fhand.write("\n];\n")
+cur.close()
+fhand.close()
+print(count, "records written to where.js")
+print("Open where.html to view the data in a browser")
